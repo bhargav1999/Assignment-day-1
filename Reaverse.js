4f608517b839ad1b7e716c6431bc8934f21e53aa
bhargav1999 <31481012+bhargav1999@users.noreply.github.com>
2022-07-28 22:32:52 +0530

    Create Reaverse.js

---
+++ Reaverse.js
@@ -0,0 +1,8 @@
+//reverse order
+//   Write a program to print a array of five names in reverses order.
+
+let names=["Bhargava","sravan","ram","varun","akbar"]
+for(let i=names.length-1;i>=0;i--)
+{
+console.log(names[i]);
+}
